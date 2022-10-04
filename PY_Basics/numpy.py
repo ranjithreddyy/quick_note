ea12373ee45b8b69a4310f5c1565678a202f553a
ranjithreddyy <86005206+ranjithreddyy@users.noreply.github.com>
2022-10-03 20:33:36 -0500

    sync code

---
+++ PY_Basics/numpy.py
@@ -0,0 +1,10 @@
+from ast import arg
+
+
+a = np.array([0,1,2,3,4])
+
+a.size # will give u size
+a.ndim
+c[0] = 100 ---can change 1st arg
+c[3:5] = [100,200] ##will chnage 3 and 4 arg 
+
---
+++ PY_Basics/python_Basics.py
@@ -0,0 +1,18 @@
+# potential code before try catch
+
+try:
+    # code to try to execute
+except ZeroDivisionError:
+    # code to execute if there is a ZeroDivisionError
+except NameError:
+    # code to execute if there is a NameError
+except:
+    # code to execute if ther is any exception
+else:
+    # code to execute if there is no exception
+finally:
+    # code to execute at the end of the try except no matter what
+    
+# code that will execute if there is no exception or a one that we are handling
+
+
